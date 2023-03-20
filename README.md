9ee6e23a2f00011eca2567c2da59bd289bfb4a61
Jake Soenneker <jake@soenneker.com>
2023-03-19 19:37:56 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Standby...
+[![](https://img.shields.io/nuget/v/Soenneker.Extensions.ServiceProvider.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.ServiceProvider/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.extensions.serviceprovider/publish.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.extensions.serviceprovider/actions/workflows/publish.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Extensions.ServiceProvider.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.ServiceProvider/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Extensions.ServiceProvider
+### A collection of useful IServiceProvider methods
+
+## Installation
+
+```
+Install-Package Soenneker.Extensions.ServiceProvider
+```
---
+++ src/ServiceProviderExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Soenneker.Extensions.ServiceProvider;
+
+public static class ServiceProviderExtension
+{
+    /// <summary>
+    /// Wraps <see cref="IServiceProvider.GetService"/> with null forgiving operand... <para/>
+    /// Microsoft.Extensions.DependencyInjection namespace isn't correctly auto detected in Visual Studio...
+    /// </summary>
+    public static T Get<T>(this IServiceProvider serviceProvider)
+    {
+        var service = serviceProvider.GetService<T>();
+
+        if (service == null)
+        {
+            throw new NullReferenceException($@"Service ({typeof(T).Name}) is not currently registered on the provider.
+                1. Check to see if the service is registered
+                2. Verify the collection on the test class is correct
+                3. Verify the constructor has the correct fixture
+                4. Verify the base class is using the correct Startup ('IntegrationTest<Startup>')
+                5. Verify the factory on the base constructor is correct");
+        }
+
+        return service;
+    }
+}
